f750f9f9414b1999a4f0c34950c8f95d8b7d8233
jxes6102 <jxes6102@gamil.com>
2023-08-23 10:21:30 +0800

    temp08231022

--- src/components/footerView.vue
+++ src/components/footerView.vue
@@ -1,19 +1,18 @@
 <template>
-    <div class="relative w-[100vw] h-[90vh] md:h-[50vh] flex justify-center items-center flex-wrap">
-        <div class="relative w-[100%] h-[80%] flex justify-center items-center flex-wrap bg-[url('/src/assets/img/green-4.jpg')] bg-cover bg-center bg-no-repeat">
-            <div class="w-[100%] md:w-[30%] h-[50%] md:h-[70%] flex justify-center items-center flex-wrap">
+    <div class="relative w-[100vw] h-auto flex justify-center items-center flex-wrap">
+        <div class="relative w-[100%] h-auto py-4 flex justify-center items-center flex-wrap bg-[url('/src/assets/img/green-4.jpg')] bg-cover bg-center bg-no-repeat">
+            <div class="w-[100%] md:w-[35%] h-auto flex justify-center items-center flex-wrap gap-y-4">
                 <div class="w-[90%] text-2xl text-black text-left">聯絡資訊</div>
-                <!-- <div class="w-[90%] h-[20%] bg-[red] flex justify-center items-center text-white">這是圖片</div> -->
                 <div class="w-[90%] text-base text-black text-left">電話：06-68916XX</div>
                 <div class="w-[90%] text-base text-black text-left">e-Mail：service@XXX-XXXX.com.tw</div>
                 <div class="w-[90%] text-base text-black text-left">地址：高雄市苓雅區四維四路167號3樓</div>
             </div>
-            <div class="w-[100%] md:w-[60%] h-[30%] md:h-[70%] flex justify-center md:items-center flex-wrap">
+            <div class="w-[100%] h-[25vh] md:w-[55%] md:h-[25vh] flex justify-center md:items-center flex-wrap">
                 <div class="w-[90%] h-auto text-2xl text-black text-left">地圖位置</div>
                 <div id="map" class="w-[90%] h-[70%] md:h-[80%] flex justify-center items-center text-white">這是地圖</div>
             </div>
         </div>
-        <div class="relative w-[100%] h-[20%] pb-[10vh] md:pb-0 bg-[#e9fddd] flex justify-center items-center flex-col">
+        <div class="relative w-[100%] h-[auto] py-2 bg-[#e9fddd] flex justify-center items-center flex-col">
             <div class="w-[100%] text-black text-sm md:text-base">Copyright © 菁鉐股份有限公司</div>
             <div class="w-[100%] text-black text-sm md:text-base">Powered by 好時資訊股份有限公司</div>
         </div>
