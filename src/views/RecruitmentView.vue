c693aba731dfe8b02bde978e1e5665fa4c9941dc
jxes6102 <jxes6102@gamil.com>
2023-08-29 11:37:39 +0800

    temp08291140

--- src/views/RecruitmentView.vue
+++ src/views/RecruitmentView.vue
@@ -2,21 +2,21 @@
     <div class="relative h-[auto] min-h-[100vh]">
         <bannerCompont></bannerCompont>
         <div class="shadow-style-1 w-[100vw] h-auto min-h-[50vh] py-4 bg-[url('/src/assets/img/green-14.jpg')] bg-cover bg-center bg-no-repeat flex justify-center items-center flex-wrap">
-            <div class="w-[100%] h-auto flex justify-center items-center flex-wrap">
-                <div class="w-[80%] h-auto">
-                    <div class="text-3xl font-bold">應徵流程</div>
-                    <div class="text-base">在菁鉐！我們提供的不只是一個工作機會，更是提供讓您有展現能力、實現夢想的舞台。不管您是懷抱夢想要實現的應屆畢業生或是想展現能力的專業人士，菁鉐都誠摯歡迎您的加入！現在立刻「前往104」查詢職務並投遞您的履歷，人資會審視職缺並主動與您連繫安排面試。</div>
+            <div class="w-[100%] h-auto flex justify-center items-center flex-wrap gap-y-10">
+                <div class="w-[80%] h-auto flex justify-center items-center flex-wrap gap-y-2">
+                    <div class="w-[100%] h-auto text-3xl font-bold">應徵流程</div>
+                    <div class="w-[100%] h-auto text-base">在菁鉐！我們提供的不只是一個工作機會，更是提供讓您有展現能力、實現夢想的舞台。不管您是懷抱夢想要實現的應屆畢業生或是想展現能力的專業人士，菁鉐都誠摯歡迎您的加入！現在立刻「前往104」查詢職務並投遞您的履歷，人資會審視職缺並主動與您連繫安排面試。</div>
                 </div>
-                <div class="w-[80%] h-auto flex justify-center items-center flex-wrap">
+                <div class="w-[80%] h-auto flex justify-center items-center flex-wrap gap-y-8">
                     <div 
-                        class="w-auto h-auto flex justify-center items-center flex-col"
+                        class="w-[100%] md:w-[25%] h-auto flex justify-center items-center flex-col"
                         v-for="(item, index) in jobData" :key="index"
                     >
-                        <div class="w-[50vw] h-[50vw] md:w-[8vw] md:h-[8vw] bg-cover bg-center bg-no-repeat" :style="{ 'background-image': 'url(' + item.url + ')' }"></div>
-                        <div class="w-[100%] h-auto flex justify-evenly items-center flex-wrap">
-                            <div class="w-[100%] h-auto text-xl md:text-xl text-[black] font-semibold">{{item.title}}</div>
-                            <div class="w-[100%] h-auto text-xl md:text-xl text-[black] font-semibold">{{item.content}}</div>
+                        <div class="w-auto h-auto p-4 border-2 border-[#7ED321] rounded-md">
+                            <div class="w-[50vw] h-[50vw] md:w-[10vw] md:h-[10vw] bg-cover bg-center bg-no-repeat" :style="{ 'background-image': 'url(' + item.url + ')' }"></div>
+                            <div class="w-[auto] h-auto text-xl md:text-xl text-[black] font-semibold">{{item.title}}</div>
                         </div>
+                        <div class="w-[auto] h-auto text-xl md:text-xl text-[#319c2e] font-semibold">{{item.content}}</div>
                     </div>
                 </div>
                 
